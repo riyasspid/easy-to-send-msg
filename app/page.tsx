fba1297d39f8ea34591c9146dbeb1d9870dfd7ce
Riyas mohammed <riyasmohammed475@gmail.com>
2026-06-05 00:31:14 +0530

    working module

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,98 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+type Messages = {
+  text :string;
+};
+
+export default function page(){
+  const [messages, setMessages] = useState<Messages[]>([]);
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+  const [clireq, setClireq] = useState<number>(1)
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL || "";
+
+  const sendMessage = async (e:any) => {
+    e.preventDefault();
+    const form = e.target
+    const formData = new FormData(form);
+    const message = formData.get("message")?.toString()
+    if(!message){
+      alert("Enter a message")
+      return;
+    }
+    else{
+      const response = await fetch(`${apiUrl}/send`,{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          text:message
+        }),
+      });
+      const data = await response.json();
+      alert(data.result);
+      if(data.result == "success"){
+        form.reset()
+      }
+    }
+   }
+   async function fetchMessage() {
+    try{
+    const response = await fetch(`${apiUrl}/fetch`,
+      {
+        method:"POST",
+        headers: {
+          "Content-Type" : "application/json"
+        },
+        body:JSON.stringify({
+          "clireq":clireq
+        })
+      }
+    )
+    const data = await response.json(); 
+    setMessages(data.messages)
+    setShowMessage(true)
+    }
+    catch(err){
+      alert(err);
+    }
+    
+   }
+  return(
+    <div className="pt-10 w-full flex flex-col justify-items-center">
+    <form onSubmit={sendMessage} className="grid grid-row justify-items-center gap-10">
+      <textarea
+      className="border rounded-xl p-2 w-100"
+      name="message"
+      placeholder="Enter your message"
+      />
+      <button
+      className="bg-black text-white h-10 w-30 rounded-3xl hover:bg-black/70 transfrom transtion-all duration-200"  
+      type="submit">submit</button>
+    </form>
+    <div className="flex">
+    <div className="flex mx-auto place-items-center h-[100px] w-[200px]">
+    <button
+    className="bg-black mx-auto mt-5 text-white h-10 w-10 rounded-3xl hover:bg-black/70 transfrom transtion-all duration-200"
+    onClick={() => setClireq(clireq+1)}>+</button>
+    <div className="mx-auto bg-black text-center place-content-center text-white text-2xl mt-5 h-20 w-20 rounded-full"><p>{clireq}</p></div>
+    <button
+    className="bg-black mx-auto mt-5 text-white h-10 w-10 rounded-3xl hover:bg-black/70 transfrom transtion-all duration-200"
+    onClick={() => {{if(clireq > 0){setClireq(clireq-1)}}}}>-</button>
     </div>
-  );
+    </div>
+    <button
+    className="bg-black mx-auto mt-5 text-white h-10 w-50 rounded-3xl hover:bg-black/70 transfrom transtion-all duration-200"
+    onClick={() => fetchMessage()}>Show all messages</button>
+    {showMessage && messages.length>0 && (<div className="flex flex-col">
+      {
+        messages.map((msg,index)=>(
+          <p className="mx-auto mt-5" key={index}>{msg.text}</p>
+        ))
+      }
+    </div>)
+    }
+    </div>
+  )
 }
